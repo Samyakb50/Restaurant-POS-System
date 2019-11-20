240b274a419807143991c4190f47a5a8f9143f0e
samyak jain <samyakb502@gmail.com>
2019-11-20 20:58:21 +0530

    Project-changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testng';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+  ngOnInit()
+  {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDCRjwZv-d10VLAN8v9_mnXt5Lf2-OEwd0",
+    authDomain: "mynew-e670b.firebaseapp.com",
+    })
+  }
 }
